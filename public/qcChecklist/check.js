b29a737702a5bcfe7c06024b65744a419ebdf51c
tannerkandor <tanner.kandor@gmail.com>
2023-08-08 10:15:50 -0500

    fix check list function

--- public/qcChecklist/check.js
+++ public/qcChecklist/check.js
@@ -1,12 +1,24 @@
-let i=0;
-function chkNext(){
-    let list = document.getElementsByClassName('chk');
+let list = document.getElementsByClassName('chk');
 
-    list[i].checked=true;
-    i++
+// let i=0;
+// function chkNext(){
+
+
+//     list[i].checked=true;
+//     i++
     
-}
+// }
 
-function resetChk(){
-    i=0
+// function resetChk(){
+//     i=0
+// }
+
+
+function chkNext(){
+    for(let i=0; i<list.length; i++){
+        if (!list[i].checked){
+            list[i].checked = true;
+            break;
+        }
+    }
 }
